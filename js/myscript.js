c947dc32e556f852bf4292096e26167f213a0240
LoDo25 <lauren@gem-advertising.com>
2015-12-03 10:40:49 -0800

    revisions

---
+++ js/myscript.js
@@ -0,0 +1,20 @@
+
+
+
+$(window).scroll(function() {
+   if ($(".navbar-default").offset().top > 70) {
+       $(".navbar-default").addClass("test");
+   } else {
+       $(".navbar-default").removeClass("test");
+   }
+});
+
+$(function() {
+   $('a.page-scroll').bind('click', function(event) {
+       var $anchor = $(this);
+       $('html, body').stop().animate({
+           scrollTop: ($($anchor.attr('href')).offset().top - 75)
+       }, 1500, 'easeInOutExpo');
+       event.preventDefault();
+   });
+});
